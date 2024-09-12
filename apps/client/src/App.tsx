28b00ef3fe3bffc84bbff6300f79b55b391f96b0
Marcin <mniedbalec@globalhub.pl>
2024-09-12 12:50:25 +0200

    init

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    fetch("api")
+      .then((response) => response.text())
+      .then((responseText) => setText(responseText));
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>{text}</h1>
+    </>
+  );
+}
+
+export default App;
